9c062179dc7c76bfdb05ddfa4e745a54cf1277ef
Deepak Bhari <deepakbhari@macbook-pro-2.lan>
2020-05-26 10:40:28 +0100

    The good Promise all based solution

--- index.js
+++ index.js
@@ -71,18 +71,30 @@ fs.readdir(process.cwd(), async (err, filenames) => {
     //END SOLUTION NUMBER 1
 
 
-    for(let filename of filenames){
-        try{
-            const stats = await lstat(filename);
+    //NON PROMISE ALL SOLUTION
+    // for(let filename of filenames){
+    //     try{
+    //         const stats = await lstat(filename);
 
-            console.log(filename, stats.isFile());
-        }catch(err){
-            console.log(err);
-        }
+    //         console.log(filename, stats.isFile());
+    //     }catch(err){
+    //         console.log(err);
+    //     }
+    // }
+    //END NON PROMISE ALL SOLUTION
 
+    const statPromises = filenames.map(filename => {
+        return lstat(filename);
+    });
+
+    const allStats = await Promise.all(statPromises);
+
+
+    for (let stats of allStats){
+        const index = allStats.indexOf(stats);
+
+        console.log(filenames[index], stats.isFile());
     }
-
-
 
 });
 
